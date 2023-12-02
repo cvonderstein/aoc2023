009c7c083d562edcc20c880e060463889819c1bd
Christian Vonderstein <cv@vonderste.in>
2023-12-02 03:59:51 +0100

    Day 1 Part 2

--- day1/README.md
+++ day1/README.md
@@ -1,5 +1,7 @@
 # Day 1
 
+The test inputs per Part is stored in this README, the input for the final result is stored in the `input` file (in this directory).
+
 ## Part 1
 
 ### Task
@@ -26,3 +28,42 @@ This directly covers the edge case (which was mentioned in the example) that the
 
 - How to iterate over lines in a file: [https://www.digitalocean.com/community/tutorials/java-read-file-line-by-line](https://www.digitalocean.com/community/tutorials/java-read-file-line-by-line)
 - ASCII Table: [https://de.wikipedia.org/wiki/American_Standard_Code_for_Information_Interchange](https://de.wikipedia.org/wiki/American_Standard_Code_for_Information_Interchange)
+
+## Part 2
+
+### Task
+
+Numbers can not only be in a digit form, but also written out as a string.
+Those count too, otherwise its the same task.
+
+Test input:
+
+```txt
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+```
+
+### Idea
+
+Create a map that contains all the replacements (e.g. key: `one` and value `1`), for every line first iterate over this map and replace all matching words.
+Afterwards do the same as before.
+
+This might be a bit overkill, but probably cleaner than some other array version of this.
+
+Update: This doesn't work like this, since there can be overlaps of two words.
+In the given example, the second line is `eightwothree`, which contains `eight`, `two` and `three`.
+The correct output for this line would be `83`, but unfortunately my attempt matches the `two` first (since the `two` comes first in the map) and therefore destroys the `eight` substring.
+So now my new attempt is to iterate over all characters in the line, if it is a digit, add this digit to a list, if it is the start of a written out digit, add the corresponding digit to the list.
+Then take this list for the later calculations.
+Using this attempt I shouldn't miss any written out number.
+
+> Quick notice: I could also iterate over all characters instead of the map and then just "destroy" later words (to not miss a first written out digit), but these later words might be required for the last digit contained, therefore I think that this is not a good option.
+
+### Help
+
+- How to iterate over a map: [https://www.baeldung.com/java-iterate-map](https://www.baeldung.com/java-iterate-map)
--- day1/day1.java
+++ day1/day1.java
@@ -3,9 +3,16 @@ package day1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class day1 {
     public static void main(String[] args) {
+        // create map for replacements
+        Map<String, Integer> numberWordMap = getStringIntegerMap();
+
         int sum = 0;
         BufferedReader reader;
 
@@ -14,13 +21,34 @@ public class day1 {
             String line = reader.readLine();
 
             while (line != null) {
-                // reduce input string to only numbers
-                String numberString = line.replaceAll("[a-z]", "");
+                // list of digits
+                List<Integer> list = new ArrayList<>();
 
-                // get first and last number of string
-                char[] asArray = numberString.toCharArray();
-                int first = asArray[0] - 0x30;
-                int last = asArray[asArray.length - 1] - 0x30;
+                System.out.printf("Line: '%s'", line);
+
+                // iterate over string, search for digits and written out digits
+                while (!line.isEmpty()) {
+                    int firstChar = line.charAt(0) - 0x30;
+                    if (firstChar > 0 && firstChar < 10) {
+                        // case first character is a digit
+                        list.add(firstChar);
+                    } else {
+                        // case it is a letter: check if it is a written out digit (and if so, add the corresponding value to the list)
+                        for (String key : numberWordMap.keySet()) {
+                            if (line.startsWith(key)) {
+                                list.add(numberWordMap.get(key));
+                            }
+                        }
+                    }
+
+                    // cut off first character (if it was a written out digit, this won't be found again)
+                    line = line.substring(1);
+                }
+                System.out.printf(", found digits: " + list + "\n");
+
+                // get first and last number of list
+                int first = list.get(0);
+                int last = list.get(list.size() - 1);
                 System.out.printf("First: %d, Last: %d\n", first, last);
 
                 // update sum
@@ -39,5 +67,20 @@ public class day1 {
 
         System.out.printf("\nFinal sum is %d.\n", sum);
 
+    }
+
+    private static Map<String, Integer> getStringIntegerMap() {
+        Map<String, Integer> numberWordMap = new HashMap<>();
+        // numberWordMap.put("zero", 0); // according to the task description, zero is not a valid value in this case
+        numberWordMap.put("one", 1);
+        numberWordMap.put("two", 2);
+        numberWordMap.put("three", 3);
+        numberWordMap.put("four", 4);
+        numberWordMap.put("five", 5);
+        numberWordMap.put("six", 6);
+        numberWordMap.put("seven", 7);
+        numberWordMap.put("eight", 8);
+        numberWordMap.put("nine", 9);
+        return numberWordMap;
     }
 }
